1ae2588c407bb23c01a2a1f9709fc573ace94e5b
r6cky <cleitonrocha321@hotmail.com>
2023-02-22 21:02:42 -0300

    add script

---
+++ script.sh
@@ -0,0 +1,17 @@
+# Instalação de libs
+
+npm init -y; echo "init -y"
+npm i -S express; echo "express"
+npm i -S pg; echo "pg"
+npm i -S dotenv; echo "dotenv"
+npm i -S pg-format; echo "pg-format"
+npm i -S express-async-errors;  echo "express-async-errors"
+npm i -S zod; echo "zod"
+npm i -S jsonwebtoken; echo "json web token"
+npm i -D ts-node-dev;  echo "ts-node-dev"
+npm i -D typescript;  echo "typescript -D"
+npm i -D @types/express; echo "@types/express"
+npm i -D @types/pg;  echo "@types/pg"
+npm i -D @types/pg-format;  echo "@types/pg-format"
+npm i -D --save-dev @types/bcryptjs echo "bcryptjs"
+npm i -D @types/jsonwebtoken; echo "jsonWebToken"
